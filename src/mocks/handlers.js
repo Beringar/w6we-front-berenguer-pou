933dad3719c2122754c2c1c9dfbf21ef76e411f0
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-20 11:23:34 +0100

    added get endpoint /robots to mock handlers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,16 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get(
+    `${process.env.REACT_APP_HEROKKU_API_URL}robots`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          { id: 1, name: "robot1" },
+          { id: 2, name: "robot2" },
+        ])
+      );
+    }
+  ),
+];
