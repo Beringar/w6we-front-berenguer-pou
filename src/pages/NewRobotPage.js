4964db18b17697a57b250e3a3c01e0bafe99c32b
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-20 18:59:21 +0100

    added boilerplate for pages new, edit, robot, robots

---
+++ src/pages/NewRobotPage.js
@@ -0,0 +1,10 @@
+const NewRobotPage = () => {
+  return (
+    <>
+      <main className="main">
+        <h1>New Robot page... aquí habrá formulario new robot</h1>
+      </main>
+    </>
+  );
+};
+export default NewRobotPage;
---
+++ src/pages/RobotPage.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Robot from "../components/Robot/Robot";
+import { useSelector, useDispatch } from "react-redux";
+import { loadRobotThunk } from "./redux/thunks/robotsThunks";
+
+const RobotPage = () => {
+  const robots = useSelector((state) => state.robots);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(loadRobotThunk(id));
+  }, [dispatch, id]);
+
+  return (
+    <>
+      <main className="main">
+        <Robot className="robot-detail" robot={robots[0]} />
+      </main>
+    </>
+  );
+};
+export default RobotPage;
