ad5f49a0b3380a93334f94270f1ebc28c0f68d38
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-20 22:13:15 +0100

    added action DeleteRobotAction

--- src/redux/actions/actionsCreators.js
+++ src/redux/actions/actionsCreators.js
@@ -9,3 +9,8 @@ export const loadRobotAction = (robot) => ({
   type: actionsTypes.loadRobot,
   robot,
 });
+
+export const deleteRobotAction = (id) => ({
+  type: actionsTypes.deleteRobot,
+  id,
+});
