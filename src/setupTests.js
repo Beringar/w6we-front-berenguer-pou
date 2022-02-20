38e92b671ab06de7e99293b325a42d40c1c0666c
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-20 15:00:29 +0100

    added custom renderWithProviders in setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,18 @@
-import "@testing-library/jest-dom";
+import { render } from "@testing-library/jest-dom";
 import { server } from "./mocks/server.js";
+import { Provider } from "react-redux";
+import store from "../src/redux/store";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+const renderWithProviders = (component) => {
+  const Providers = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return render(component, { wrapper: Providers });
+};
+
+export default renderWithProviders;
