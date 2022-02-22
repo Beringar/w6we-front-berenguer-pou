d897cd39e8e9e07b6ef28c14b33a8e4b678c4c8e
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-22 08:12:25 +0100

    added login form with no dispatching to loginthunk(yet)

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <main className="main">
+        <LoginForm />
+      </main>
+    </>
+  );
+};
+export default LoginPage;
