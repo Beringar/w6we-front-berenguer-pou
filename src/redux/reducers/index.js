3f1fb6bd8d5397c64da4d67d144c9aa9d4e164a4
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-22 18:46:32 +0100

    add user to rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import robotsReducers from "./robotsReducers";
 import robotReducers from "./robotReducers";
+import userReducers from "./userReducers";
 
 const rootReducer = combineReducers({
   robots: robotsReducers,
   robot: robotReducers,
+  user: userReducers,
 });
 
 export default rootReducer;
